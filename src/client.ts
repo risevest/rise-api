53acf24df833bd55050b83e211476b3243730184
balogunofafrica <balogunakanbi.k@gmail.com>
2024-08-13 16:28:36 +0100

    add client implementation

---
+++ src/client.ts
@@ -0,0 +1,226 @@
+import { Static, TSchema } from "@sinclair/typebox";
+import { Value } from "@sinclair/typebox/value";
+import {
+  useMutation,
+  UseMutationOptions,
+  UseMutationResult,
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from "react-query";
+
+import type {
+  Fetcher,
+  HttpMethod,
+  MaybeOptionalArg,
+  MaybeOptionalOptions,
+} from "./client.types.ts";
+import {
+  DeleteEndpoints,
+  EndpointByMethod,
+  GetEndpoints,
+  PatchEndpoints,
+  PostEndpoints,
+} from "./contract.js";
+
+export class RiseApiClient {
+  baseUrl: string = "";
+
+  constructor(public fetcher: Fetcher) {}
+
+  setBaseUrl(baseUrl: string) {
+    this.baseUrl = baseUrl;
+    return this;
+  }
+
+  parse<T extends TSchema>(schema: T, value: unknown) {
+    return Value.Parse(schema, value);
+  }
+
+  parseAsync<T extends TSchema>(schema: T, value: Promise<unknown>) {
+    return value.then((res) => this.parse(schema, res));
+  }
+
+  post<Path extends keyof PostEndpoints, TEndpoint extends PostEndpoints[Path]>(
+    path: Path,
+    ...params: MaybeOptionalArg<Static<TEndpoint>["parameters"]>
+  ): Promise<Static<TEndpoint>["response"]> {
+    const PostSchema = EndpointByMethod.post[path].properties;
+
+    return this.parseAsync(
+      PostSchema.response,
+      this.fetcher(
+        "post",
+        this.baseUrl + path,
+        this.parse(PostSchema.parameters, params[0])
+      )
+    );
+  }
+
+  get<Path extends keyof GetEndpoints, TEndpoint extends GetEndpoints[Path]>(
+    path: Path,
+    ...params: MaybeOptionalArg<Static<TEndpoint>["parameters"]>
+  ): Promise<Static<TEndpoint>["response"]> {
+    const GetSchema = EndpointByMethod.get[path].properties;
+
+    return this.parseAsync(
+      GetSchema.response,
+      this.fetcher(
+        "get",
+        this.baseUrl + path,
+        this.parse(GetSchema.parameters, params[0])
+      )
+    );
+  }
+
+  patch<
+    Path extends keyof PatchEndpoints,
+    TEndpoint extends PatchEndpoints[Path]
+  >(
+    path: Path,
+    ...params: MaybeOptionalArg<Static<TEndpoint>["parameters"]>
+  ): Promise<Static<TEndpoint>["response"]> {
+    const PatchSchema = EndpointByMethod.patch[path].properties;
+
+    return this.parseAsync(
+      PatchSchema.response,
+      this.fetcher(
+        "patch",
+        this.baseUrl + path,
+        this.parse(PatchSchema.parameters, params[0])
+      )
+    );
+  }
+  delete<
+    Path extends keyof DeleteEndpoints,
+    TEndpoint extends DeleteEndpoints[Path]
+  >(
+    path: Path,
+    ...params: MaybeOptionalArg<Static<TEndpoint>["parameters"]>
+  ): Promise<Static<TEndpoint>["response"]> {
+    const PatchSchema = EndpointByMethod.delete[path].properties;
+
+    return this.parseAsync(
+      PatchSchema.response,
+      this.fetcher(
+        "delete",
+        this.baseUrl + path,
+        this.parse(PatchSchema.parameters, params[0])
+      )
+    );
+  }
+
+  getRequestKey<
+    Method extends HttpMethod,
+    Endpoint extends EndpointByMethod[Method],
+    Path extends keyof Endpoint,
+    TEndpoint extends Endpoint[Path]
+  >(
+    method: Method,
+    path: Path,
+    //   @ts-expect-error cannot seem to index with parameters
+    ...params: MaybeOptionalArg<Static<TEndpoint>["parameters"]>
+  ): readonly [string, ...unknown[]] {
+    const key = `${method}_${path as string}`;
+    return [key, ...params];
+  }
+}
+
+export function createRiseApiClient(fetcher: Fetcher, baseUrl?: string) {
+  return new RiseApiClient(fetcher).setBaseUrl(baseUrl ?? "");
+}
+
+export class RiseApiHooks {
+  #client: RiseApiClient;
+  constructor(client: RiseApiClient) {
+    this.#client = client;
+  }
+
+  public useGet<
+    Path extends keyof GetEndpoints,
+    TEndpoint extends GetEndpoints[Path],
+    TData extends Static<TEndpoint>["response"],
+    TError = unknown
+  >(
+    path: Path,
+    ...rest: MaybeOptionalOptions<
+      Static<TEndpoint>["parameters"],
+      Omit<UseQueryOptions<TData, TError>, "queryKey" | "queryFn">
+    >
+  ): UseQueryResult<TData, Error> {
+    const [config, options] = rest;
+
+    return useQuery({
+      queryFn: () => this.#client.get(path, config as never),
+      queryKey: this.#client.getRequestKey("get", path, config as never),
+      ...(options as {}),
+    });
+  }
+
+  public usePost<
+    Path extends keyof PostEndpoints,
+    TEndpoint extends PostEndpoints[Path],
+    TData extends Static<TEndpoint>["response"],
+    TError = unknown
+  >(
+    path: Path,
+    ...rest: MaybeOptionalOptions<
+      Static<TEndpoint>["parameters"],
+      Omit<UseMutationOptions<TData, TError>, "mutationKey" | "mutationFn">
+    >
+  ): UseMutationResult<TData, Error> {
+    const [config, options] = rest;
+
+    return useMutation({
+      mutationFn: () => this.#client.post(path, config as never),
+      mutationKey: this.#client.getRequestKey("post", path, config as never),
+      ...(options as {}),
+    }) as never;
+  }
+
+  public usePatch<
+    Path extends keyof PatchEndpoints,
+    TEndpoint extends PatchEndpoints[Path],
+    TData extends Static<TEndpoint>["response"],
+    TError = unknown
+  >(
+    path: Path,
+    ...rest: MaybeOptionalOptions<
+      Static<TEndpoint>["parameters"],
+      Omit<UseMutationOptions<TData, TError>, "mutationKey" | "mutationFn">
+    >
+  ): UseMutationResult<TData, Error> {
+    const [config, options] = rest;
+
+    return useMutation({
+      mutationFn: () => this.#client.patch(path, config as never),
+      mutationKey: this.#client.getRequestKey("patch", path, config as never),
+      ...(options as {}),
+    }) as never;
+  }
+
+  public useDelete<
+    Path extends keyof DeleteEndpoints,
+    TEndpoint extends DeleteEndpoints[Path],
+    TData extends Static<TEndpoint>["response"],
+    TError = unknown
+  >(
+    path: Path,
+    ...rest: MaybeOptionalOptions<
+      Static<TEndpoint>["parameters"],
+      Omit<UseMutationOptions<TData, TError>, "mutationKey" | "mutationFn">
+    >
+  ): UseMutationResult<TData, Error> {
+    const [config, options] = rest;
+
+    return useMutation({
+      mutationFn: () => this.#client.delete(path, config as never),
+      mutationKey: this.#client.getRequestKey("delete", path, config as never),
+      ...(options as {}),
+    }) as never;
+  }
+}
+
+export function createRiseApiHooks(client: RiseApiClient) {
+  return new RiseApiHooks(client);
+}
---
+++ src/client.types.ts
@@ -0,0 +1,20 @@
+import type { Endpoint, EndpointParameters, Method } from "./contract.ts";
+
+export type HttpMethod = "post" | "get" | "patch" | "delete";
+export type Fetcher = (
+  method: Method,
+  url: string,
+  parameters?: EndpointParameters | undefined
+) => Promise<Endpoint["response"]>;
+
+export type RequiredKeys<T> = {
+  [P in keyof T]-?: undefined extends T[P] ? never : P;
+}[keyof T];
+
+export type MaybeOptionalArg<T> = RequiredKeys<T> extends never
+  ? [config?: T]
+  : [config: T];
+
+export type MaybeOptionalOptions<T, O> = RequiredKeys<T> extends never
+  ? [config?: T, queryOptions?: O]
+  : [config: T, queryOptions?: O];
