8c12583c431fae3a92b2ad235959dd1cce10049c
balogunofafrica <balogunakanbi.k@gmail.com>
2024-08-13 16:24:54 +0100

    add binary

---
+++ bin/cli.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+OUTPUT_FILE="./src/contract.ts"
+
+generate_file() {
+  local INPUT_FILE="$1"
+
+  if [ -z "$INPUT_FILE" ]; then
+    echo "Error: input swagger is required for the generate command."
+    exit 1
+  fi
+  
+  npx typed-openapi "$INPUT_FILE" -o "$OUTPUT_FILE" -r typebox
+}
+
+display_help() {
+  echo "Usage: rise-api [command] [options]"
+  echo ""
+  echo "Commands:"
+  echo "  generate [content]  Generate a file with the specified content"
+  echo "  help                Display this help message"
+}
+
+case "$1" in
+  generate)
+    generate_file "$2"
+    ;;
+  help)
+    display_help
+    ;;
+  *)
+    echo "Error: Unknown command '$1'"
+    display_help
+    exit 1
+    ;;
+esac
